76e1beca43ea6783aeaf3161ed26ff84d5845612
SamuraiBread <samuraibreadi@gmail.com>
2025-08-26 10:03:52 +0200

    HSMP: Devine Stone v0.0.101 (Alpha)
    - Added infoabout menu
    - Added nerdinfo addonversion menu
    - Added nerdinfo latestversion menu
    - Added factions menu
    - Added dependency for RP
    - Added main menu icons

--- BP/scripts/play_button.js
+++ BP/scripts/play_button.js
@@ -8,13 +8,16 @@ system.beforeEvents.startup.subscribe((e) => {
             const block = e.block
             const menu = new ActionFormData()
                 .title('Main Menu')
-                .body('')
+                .body('§e§lWelcome to HiraethSMP!§r An §eNation based Minecraft:Bedrock Edition SMP§r with a lot to do!' +
+                    "\n\nExplore the §e10k by 10k map§r made by §2SamuraiBread§r - An owner of the SMP itself!" +
+                    "\n\nHave fun trying out §emagical properties of special items§r added to the game!" +
+                    "\n\nEnjoy §efree Addon download and support§r, plus §ebe the one who will grow HiraethSMP addon§r with us!")
                 .label('Game')
-                .button('Play Case 0')
+                .button('Play Case 0', "textures/ui/hsmp_mainmenu/hsmp_play_button")
 
                 .label('Informative Section')
                 .divider()
-                .button('Information Case 1')
+                .button('Information Case 1', "textures/ui/hsmp_mainmenu/info_button")
                 .button('Factions Case 2')
                 .button('Credits Case 3')
                 .divider()
@@ -31,6 +34,53 @@ system.beforeEvents.startup.subscribe((e) => {
                 .button('Latest Update Case 2')
                 .divider()
                 .button('Back Case 3');
+
+            const infoabout = new ActionFormData()
+                .title('About HiraethSMP')
+                .body('What is HiraethSMP? How it was created and what is it`s story?\n\nHere you will read about everything about HiraethSMP')
+                .label('So, what is HiraethSMP about?')
+                .divider()
+                .label('HiraethSMP is mostly about factions, with a little plot twist.\n\nIt is a world with a story and horror parts of it.' +
+                    '\n\nIt`s mission? Make you cry at the end :).' + '\n\nIt`s connected with my own life expierience. So consider it a story within a story.')
+                .divider()
+                .button('Back');
+
+            const nerdinfo_addonversion = new ActionFormData()
+                .title('Addon Version')
+                .body('Bridge Build: §ev2.7.50§r')
+                .divider()
+                .label('Addon Version: §sv0.0.100§r')
+                .divider()
+                .label('Colour Meaning:\n\n' +
+                    '§2Green§r - Bigger update\n' + '§sCyan§r - Smaller update.\n' +
+                    '§eYellow§r - Bridge version, newer versions means a lot more possibilities.')
+                .divider()
+                .button('Back')
+
+            const nerdinfo_latestupdate = new ActionFormData()
+                .title('Latest Update')
+                .body('v0.0.100 Built with "bridge. v2" v2.7.50')
+                .divider()
+                .label(
+                    "What's new?\n\n" +
+                    "- Added Main Menu\n" +
+                    "- Added Play button"
+                )
+                .divider()
+                .button('Back');
+
+            const factions = new ActionFormData()
+                .title('Factions')
+                .body('')
+                .divider()
+                .label('Faction Information')
+                .button('Sunlight Case 0')
+                .button('Crimsonhold Case 1')
+                .button('Witherhood Case 2')
+                .button('Red Lotus Case 3')
+                .divider()
+                .label('Go back to Main Menu')
+                .button('Back');
 
             const credits = new ActionFormData()
                 .title('Credits')
@@ -54,6 +104,7 @@ system.beforeEvents.startup.subscribe((e) => {
                             break;
 
                         case 2:
+                            openfacinfo(source)
                             break;
 
                         case 3:
@@ -70,10 +121,13 @@ system.beforeEvents.startup.subscribe((e) => {
                 info.show(source).then((r) => {
                     switch (r.selection) {
                         case 0:
+                            openinfoabout(source)
                             break;
                         case 1:
+                            openinfoversion(source)
                             break;
                         case 2:
+                            openinfolatestupdate(source)
                             break;
                         case 3:
                             openmenu(source)
@@ -83,7 +137,69 @@ system.beforeEvents.startup.subscribe((e) => {
                             break;
                     }
                 })
+            };
+
+            function openinfoabout(source) {
+                infoabout.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            openinfo(source)
+                            break;
+                        default:
+                            openinfo(source)
+                            break;
+                    }
+                })
+            };
+
+            function openinfoversion(source) {
+                nerdinfo_addonversion.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            openinfo(source)
+                            break;
+                        default:
+                            openinfo(source)
+                            break;
+                    }
+                })
+            };
+
+            function openinfolatestupdate(source) {
+                nerdinfo_latestupdate.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            openinfo(source)
+                            break;
+
+                        default:
+                            openinfo(source)
+                            break;
+                    }
+                })
             }
+
+            function openfacinfo(source) {
+                factions.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            break;
+
+                        case 1:
+                            break;
+
+                        case 3:
+                            break;
+
+                        case 4:
+                            openmenu(source)
+                            break;
+
+                        default:
+                            openmenu(source)
+                    }
+                })
+            };
 
             function opencredits(source) {
                 credits.show(source).then((r) => {
@@ -92,7 +208,7 @@ system.beforeEvents.startup.subscribe((e) => {
                             openmenu(source)
                     }
                 })
-            }
+            };
 
             openmenu(source)
         },
