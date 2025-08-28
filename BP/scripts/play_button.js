5159bc4c9a2da04fd87ddbfc5e25a69e53a13582
SamuraiBread <samuraibreadi@gmail.com>
2025-08-28 15:38:58 +0200

    HiraethSMP: The Devine Stone v1.0.103 - Alpha
    - Finished menu

--- BP/scripts/play_button.js
+++ BP/scripts/play_button.js
@@ -24,6 +24,84 @@ system.beforeEvents.startup.subscribe((e) => {
                 .label('Explore')
                 .button('Explore the Spawn Case 4', "textures/ui/hsmp_mainmenu/back_button");
 
+            const play_choosefac = new ActionFormData()
+                .title('Faction Choosing')
+                .body("First, let's start by choosing a faction.")
+                .divider()
+                .label('Avaible Factions')
+                .button('Sunlight Faction C0')
+                .button('Crimsonhood Faction C1')
+                .button('Witherhood Faction C2')
+                .button('Red Lotus Faction C3')
+                .divider()
+                .button('Cancel C4');
+
+            function getFactionName(source) {
+                if (source.hasTag('sunlight_faction')) return '§e§lSunlight§r';
+                if (source.hasTag('crimsonhood_faction')) return '§l§mCrimsonhood§r';
+                if (source.hasTag('witherhood_faction')) return '§5§lWitherhood§r';
+                if (source.hasTag('redlotus_faction')) return '§c§lRed Lotus§r';
+                return '§7§oNo Faction Selected§r';
+            }
+
+            function createFactionConfirmForm(source) {
+                const factionName = getFactionName(source);
+
+                return new ActionFormData()
+                    .title('Confirm Your Faction')
+                    .body('/!\\ To Change a Faction you\'ll need to contact our staff team. /!\\\n\n' + 'Are you sure you want to join ' + factionName + ' Faction?')
+                    .divider()
+                    .button('Confirm')
+                    .button('Cancel');
+            }
+
+            const play_chooserace = new ActionFormData()
+                .title('Choose Your Race')
+                .body('\t /!\\ Races cannot be changed! /!\\ ')
+                .divider()
+                .button('Human')
+                .divider()
+                .label('Pros:\n- Balanced physical aspects\n- Can wear Heavy Duty Armor\n- Can become a Mage\n- Adapts to any environment\n- Trades more effectively (-20 coins on the price of the item)\n\nCons:\n- As a Mage, has reduced mana\n- Wearing Heavy Duty Armor noticeably decreases speed\n- No vision-related advantages\n- The Divine Stone has a 20% chance of killing Humans on touch\n- Stamina regenerates slower than Elves or Half-Orcs')
+                .divider()
+                .button('Elf')
+                .divider()
+                .label('Pros:\n- Night Vision\n- Strongest magical ability (more Mana)\n- Bows automatically gain Infinity upon crafting\n- Faster than other races\n- Natural resistance to poison\n\nCons:\n- Cannot wear heavy armor (Iron, Diamond, Netherite)\n- Weaker body: takes more damage from swords, axes, and spears\n- Cannot stay in stone structures for too long, causes discomfort\n- Mining speed is slower underground')
+                .divider()
+                .button('Half-Orc')
+                .divider()
+                .label('Pros:\n- Permanent Strength I\n- More resilient to damage\n- Deals more melee damage with axes and swords\n- Natural knockback resistance\n- Health regenerates faster than Humans and Elves\n\nCons:\n- Cannot use advanced magic\n- Slowest race (permanent Slowness I)\n- Higher Prices')
+                .divider()
+                .label('There is no going back from this menu. Sorry.')
+
+            function getPlayerRace(source) {
+                if (source.hasTag('human')) return 'Human';
+                if (source.hasTag('elf')) return 'Elf';
+                if (source.hasTag('halforc')) return 'Half Orc';
+                return 'Error Found: No race detected. Contact SamuraiBread.';
+            }
+
+            function createRaceConfirmForm(source) {
+                const playerRace = getPlayerRace(source);
+
+                return new ActionFormData()
+                    .title('Confirm Your Race')
+                    .body('Are you sure you want to become an ' + playerRace + ' ?')
+                    .divider()
+                    .button('Continue')
+                    .button('Cancel');
+            }
+
+            function createFinalizationForm(source) {
+                const playerRace = getPlayerRace(source);
+                const factionName = getFactionName(source);
+
+                return new ActionFormData()
+                    .title('Finalization')
+                    .body('You\'re an ' + playerRace + ' from ' + factionName + ' Faction. You don\'t remember the past')
+                    .divider()
+                    .button('Okay');
+            }
+
             const info = new ActionFormData()
                 .title('Information')
                 .body('Here you will find all info needed for you :)')
@@ -82,6 +160,35 @@ system.beforeEvents.startup.subscribe((e) => {
                 .label('Go back to Main Menu')
                 .button('Back');
 
+            const factioninfo_Sunlight = new ActionFormData()
+                .title('Sunlight Faction')
+                .body(
+                    'Sunlight Faction\n\n' +
+                    'The story is yet to be written by YOU.'
+                )
+                .divider()
+                .button('Back');
+
+            const factioninfo_Crimson = new ActionFormData()
+                .title('Crimsonhold Faction')
+                .body('Crimsonhold Faction\n\n' + 'The story is yet to be written by YOU.')
+                .divider()
+                .button('Back');
+
+            const factioninfo_Witherhood = new ActionFormData()
+                .title('Witherhood Faction')
+                .body('Witherhood Faction\n\n' + 'The story is yet to be written by YOU.')
+                .divider()
+                .button('Back');
+
+            const factioninfo_Red = new ActionFormData()
+                .title('Red Lotus Faction')
+                .body('Red Lotus Faction\n\n' + 'The story is yet to be written by YOU.')
+                .divider()
+                .button('Back');
+
+
+
             const credits = new ActionFormData()
                 .title('Credits')
                 .divider()
@@ -97,6 +204,7 @@ system.beforeEvents.startup.subscribe((e) => {
                 menu.show(source).then((r) => {
                     switch (r.selection) {
                         case 0:
+                            play_page1(source)
                             break;
 
                         case 1:
@@ -116,6 +224,119 @@ system.beforeEvents.startup.subscribe((e) => {
                     }
                 })
             };
+
+            function play_page1(source) {
+                play_choosefac.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            source.addTag('sunlight_faction')
+                            faction_confirm(source)
+                            break;
+
+                        case 1:
+                            source.addTag('crimsonhood_faction')
+                            faction_confirm(source)
+                            break;
+
+                        case 2:
+                            source.addTag('witherhood_faction')
+                            faction_confirm(source)
+                            break;
+
+                        case 3:
+                            source.addTag('redlotus_faction')
+                            faction_confirm(source)
+                            break;
+
+                        case 4:
+                            openmenu(source)
+                            break;
+
+                        default:
+                            openmenu(source)
+                            break;
+                    }
+                })
+            }
+
+            function faction_confirm(source) {
+                const fac_confirm = createFactionConfirmForm(source);
+
+                fac_confirm.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            choose_race(source)
+                            break;
+
+                        case 1:
+                            source.removeTag('sunlight_faction');
+                            source.removeTag('crimsonhood_faction');
+                            source.removeTag('witherhood_faction');
+                            source.removeTag('redlotus_faction');
+                            play_page1(source);
+                            break;
+                    }
+                });
+            }
+
+            function choose_race(source) {
+                play_chooserace.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            source.addTag('human')
+                            race_confirm(source)
+                            break;
+
+                        case 1:
+                            source.addTag('elf')
+                            race_confirm(source)
+                            break;
+                        case 2:
+                            source.addTag('halforc')
+                            race_confirm(source)
+                            break;
+
+                        default:
+                            choose_race(source)
+                    }
+                })
+            }
+
+            function race_confirm(source) {
+                const r_confirm = createRaceConfirmForm(source);
+
+                r_confirm.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            finalization(source)
+                            break;
+
+                        case 1:
+                            source.removeTag('human')
+                            source.removeTag('elf')
+                            source.removeTag('halforc')
+                            choose_race(source)
+                            break;
+
+                        default:
+                            source.removeTag('human')
+                            source.removeTag('elf')
+                            source.removeTag('halforc')
+                            choose_race(source)
+                            break;
+
+
+
+                    }
+                })
+            }
+
+            function finalization(source) {
+                const finalization_form = createFinalizationForm(source)
+
+                finalization_form.show(source)
+            }
+
 
             function openinfo(source) {
                 info.show(source).then((r) => {
@@ -183,12 +404,19 @@ system.beforeEvents.startup.subscribe((e) => {
                 factions.show(source).then((r) => {
                     switch (r.selection) {
                         case 0:
+                            openfacinfo_sunlight(source)
                             break;
 
                         case 1:
+                            openfacinfo_crimson(source)
+                            break;
+
+                        case 2:
+                            openfacinfo_wither(source)
                             break;
 
                         case 3:
+                            openfacinfo_red(source)
                             break;
 
                         case 4:
@@ -197,9 +425,69 @@ system.beforeEvents.startup.subscribe((e) => {
 
                         default:
                             openmenu(source)
+                            break;
                     }
                 })
             };
+
+            function openfacinfo_sunlight(source) {
+                factioninfo_Sunlight.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            openfacinfo(source)
+                            break;
+
+                        default:
+                            openfacinfo(source)
+                            break;
+                    }
+                })
+            }
+
+            function openfacinfo_crimson(source) {
+                factioninfo_Crimson.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            openfacinfo(source)
+                            break;
+
+                        default:
+                            openfacinfo(source)
+                            break;
+                    }
+                })
+            }
+
+            function openfacinfo_wither(source) {
+                factioninfo_Witherhood.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            openfacinfo(source)
+                            break;
+
+                        default:
+                            openfacinfo(source)
+                            break;
+                    }
+                })
+            }
+
+            function openfacinfo_red(source) {
+                factioninfo_Red.show(source).then((r) => {
+                    switch (r.selection) {
+                        case 0:
+                            openfacinfo(source)
+                            break;
+
+                        default:
+                            openfacinfo(source)
+                            break;
+                    }
+                })
+            }
+
+
+
 
             function opencredits(source) {
                 credits.show(source).then((r) => {
@@ -209,7 +497,6 @@ system.beforeEvents.startup.subscribe((e) => {
                     }
                 })
             };
-
             openmenu(source)
         },
     })
