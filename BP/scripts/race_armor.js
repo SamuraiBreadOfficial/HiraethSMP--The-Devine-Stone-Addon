4ffcc833f23dbf23fd435e152355c80995172562
SamuraiBread <samuraibreadi@gmail.com>
2025-09-25 16:10:05 +0200

    Helmet now changes into race defined helmet

--- BP/scripts/race_armor.js
+++ BP/scripts/race_armor.js
@@ -1,4 +1,4 @@
-import { system } from "@minecraft/server";
+import { system, world, ItemStack } from "@minecraft/server";
 
 system.beforeEvents.startup.subscribe((e) => {
     e.itemComponentRegistry.registerCustomComponent('hsmp:helmet_use', {
@@ -13,4 +13,33 @@ system.beforeEvents.startup.subscribe((e) => {
             }
         }
     })
+});
+
+world.afterEvents.itemUse.subscribe(e => {
+    const p = e.source;
+    const i = e.itemStack.typeId;
+    const inv = p.getComponent("minecraft:inventory")?.container;
+    const slot = p.selectedSlotIndex;
+    const elfLHelmID = "hsmp:elf_leather_armor_helmet";
+
+    if (p.hasTag("elf") && i === "minecraft:leather_helmet") {
+        system.run(() => {
+            const originalItem = inv.getItem(slot);
+            if (!originalItem) return;
+
+            const enchComp = originalItem.getComponent("minecraft:enchantments");
+            const enchantments = enchComp?.enchantments;
+
+            const elfHelmet = new ItemStack(elfLHelmID, 1);
+
+            if (enchantments && enchantments.length > 0) {
+                elfHelmet.setComponent("minecraft:enchantments", {
+                    enchantments: enchantments.map(e => ({ id: e.id, level: e.level }))
+                });
+            }
+
+            inv.setItem(slot, elfHelmet);
+            console.warn("Set Helmet to Race Defined")
+        });
+    }
 });
