7fd7432f68bb19977f0d4f1f38483593c6b60276
SamuraiBread <samuraibreadi@gmail.com>
2025-10-07 19:13:59 +0200

    Added mod and admin menu

--- BP/scripts/modules/admin/admin_menu.js
+++ BP/scripts/modules/admin/admin_menu.js
@@ -1,14 +1,17 @@
 import { world, system } from "@minecraft/server"
 import { ActionFormData } from "@minecraft/server-ui"
 
-import { logBuffer, loadLogs, saveLogs, antiGriefBuffer, isOwner, adminList, adminMenuPerm, modList } from "../index.js"
+import { logBuffer, loadLogs, saveLogs, antiGriefBuffer, isOwner, adminList, adminMenuPerm, modList, clearLogs } from "../index.js"
 
 // Admin Menu
 system.beforeEvents.startup.subscribe(ev => {
     ev.itemComponentRegistry.registerCustomComponent('hsmp:admin_menu', {
         onUse: ev => {
+            // Register source and source name. Note: source = Player who used the item. 
+
             const source = ev.source
             const sourceName = ev.source.name
+
 
             // Register Item
             const itemId = ev.itemStack.typeId
@@ -24,8 +27,8 @@ system.beforeEvents.startup.subscribe(ev => {
 
             //Menu Functions
             function admin_menu(source) {
-                // Register source and source name. Note: source = Player who used the item. 
 
+                // If without permission, will log the try to use the item
                 if (!menuperm) {
                     // Register Logs
                     const time = new Date().toLocaleTimeString();
@@ -46,9 +49,12 @@ system.beforeEvents.startup.subscribe(ev => {
                         .header('Staff Menu')
                         .label("Type: Owner Menu")
                         .label(`Hello ${sourceName}!`)
-                        .label('Choose an Action')
+                        .label('Logging')
                         .button('Show Logs')
                         .button('Show AntiGrief\nLogs')
+                        .button('Clear All Logs')
+                        .label('Admin Tools')
+                        .button('Vanish')
                         .show(source).then(r => {
                             switch (r.selection) {
                                 case 0:
@@ -59,12 +65,94 @@ system.beforeEvents.startup.subscribe(ev => {
                                     antigrief(source)
                                     break;
 
+                                case 2:
+                                    clearLogs()
+                                    source.sendMessage('All Logs Cleared')
+
+                                    const time = new Date().toLocaleTimeString();
+                                    const date = new Date().toLocaleDateString();
+                                    const logEntry = `[ INFO ] All logs cleared by ${sourceName} at ${time} | ${date}`
+
+                                    console.info(logEntry);
+                                    logBuffer.push(logEntry)
+                                    saveLogs()
+                                    break;
+
+                                case 3:
+                                    vanish(source);
+                                    admin_menu(source)
+                                    break;
+
                                 default:
                                     break;
                             }
                         })
 
                 }
+                if (menuperm && admin) {
+                    return new ActionFormData()
+                        .title('')
+                        .header('Staff Menu')
+                        .label("Type: Admin Menu")
+                        .label(`Hello ${sourceName}!`)
+                        .label('Logging')
+                        .button('Show Logs')
+                        .button('Show AntiGrief\nLogs')
+                        .label('Admin Tools')
+                        .button('Vanish')
+                        .show(source).then(r => {
+                            switch (r.selection) {
+                                case 0:
+                                    logs(source);
+                                    break;
+
+                                case 1:
+                                    antigrief(source)
+                                    break;
+
+                                case 2:
+                                    vanish(source);
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        })
+
+                }
+                if (menuperm && mod) {
+                    return new ActionFormData()
+                        .title('')
+                        .header('Staff Menu')
+                        .label("Type: Mod Menu")
+                        .label(`Hello ${sourceName}!`)
+                        .label('Logging')
+                        .button('Show Logs')
+                        .button('Show AntiGrief\nLogs')
+                        .label('Admin Tools')
+                        .button('Vanish')
+                        .show(source).then(r => {
+                            switch (r.selection) {
+                                case 0:
+                                    logs(source);
+                                    break;
+
+                                case 1:
+                                    antigrief(source)
+                                    break;
+
+                                case 2:
+                                    vanish(source);
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        })
+
+                }
+
+
             }
 
 
@@ -102,6 +190,20 @@ system.beforeEvents.startup.subscribe(ev => {
                     .label(logs)
                     .show(source)
             }
+
+            function vanish(source) {
+                if (source.hasTag('vanish')) {
+                    source.runCommand('tag @s remove vanish')
+                    source.runCommand('effect @s clear')
+                }
+                else {
+                    source.runCommand(`tellraw @a {"rawtext":[{"text":"§e${sourceName} left the server."}]}`)
+                    source.runCommand('effect @s invisibility infinite 0 true')
+                    source.runCommand('tag @s add vanish')
+                }
+
+            }
+
 
             // Use without permission
             admin_menu(source)
