9efeba3d31b459433da322f2e0d32085ed0f9da1
SamuraiBread <samuraibreadi@gmail.com>
2025-10-07 18:12:38 +0200

    Changed antigrief last logs to 100 and logs to 50

--- BP/scripts/modules/admin/admin_menu.js
+++ BP/scripts/modules/admin/admin_menu.js
@@ -7,9 +7,6 @@ import { logBuffer, loadLogs, saveLogs, antiGriefBuffer, isOwner, adminList, adm
 system.beforeEvents.startup.subscribe(ev => {
     ev.itemComponentRegistry.registerCustomComponent('hsmp:admin_menu', {
         onUse: ev => {
-
-            // Register source and source name. Note: source = Player who used the item. 
-
             const source = ev.source
             const sourceName = ev.source.name
 
@@ -25,54 +22,90 @@ system.beforeEvents.startup.subscribe(ev => {
 
             const menuperm = adminMenuPerm.includes(sourceName)
 
-            // Use without permission
+            //Menu Functions
+            function admin_menu(source) {
+                // Register source and source name. Note: source = Player who used the item. 
 
-            if (!menuperm) {
-                // Register Logs
-                const time = new Date().toLocaleTimeString();
-                const date = new Date().toDateString();
-                const logEntry = `§c[ No Permission ${time} | ${date} ]§r §e${sourceName}§r used §e${itemId}§c but doesn't have permission!`
+                if (!menuperm) {
+                    // Register Logs
+                    const time = new Date().toLocaleTimeString();
+                    const date = new Date().toDateString();
+                    const logEntry = `§c[ No Permission ${time} | ${date} ]§r §e${sourceName}§r used §e${itemId}§c but doesn't have permission!`
 
-                // Send Feedback Message
-                source.sendMessage(`You don't have a permission to use this item. This has been saved to logs.`)
+                    // Send Feedback Message
+                    source.sendMessage(`You don't have a permission to use this item. This has been saved to logs.`)
 
-                // Push Logs
-                console.warn(logEntry);
-                logBuffer.push(logEntry);
-                saveLogs();
+                    // Push Logs
+                    console.warn(logEntry);
+                    logBuffer.push(logEntry);
+                    saveLogs();
+                }
+                if (menuperm && owner) {
+                    return new ActionFormData()
+                        .title('')
+                        .header('Staff Menu')
+                        .label("Type: Owner Menu")
+                        .label(`Hello ${sourceName}!`)
+                        .label('Choose an Action')
+                        .button('Show Logs')
+                        .button('Show AntiGrief\nLogs')
+                        .show(source).then(r => {
+                            switch (r.selection) {
+                                case 0:
+                                    logs(source);
+                                    break;
+
+                                case 1:
+                                    antigrief(source)
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        })
+
+                }
             }
-            if (menuperm && owner) {
+
+
+            function logs(source) {
+                loadLogs()
+
+                const logs = logBuffer.slice(-50).reverse().join('\n\n')
+
                 return new ActionFormData()
                     .title('')
-                    .header('Staff Menu')
-                    .label("Type: Owner Menu")
-                    .label(`Hello ${sourceName}!`)
-                    .label('Choose an Action')
-                    .button('Show Logs')
-                    .button('Show AntiGrief\nLogs')
+                    .body('')
+                    .button('Back')
+                    .header('Last 50 logs.')
+                    .label(logs)
                     .show(source).then(r => {
                         switch (r.selection) {
                             case 0:
-                                logs(source)
+                                admin_menu(source)
+
+                            default:
                         }
                     })
-
             }
 
+            function antigrief(source) {
+                loadLogs()
+
+                const logs = antiGriefBuffer.slice(-100).reverse().join('\n\n');
+
+                return new ActionFormData()
+                    .title('')
+                    .body("")
+                    .button('Back')
+                    .header('Antigrief last 100 warns.')
+                    .label(logs)
+                    .show(source)
+            }
+
+            // Use without permission
+            admin_menu(source)
         }
     })
 })
 
-//Menu Functions
-
-function logs(source) {
-    loadLogs()
-
-    const logs = logBuffer.slice(-5).reverse().join('\n\n')
-
-    return new ActionFormData()
-        .title('test')
-        .body(logs)
-        .button('')
-        .show(source)
-}
