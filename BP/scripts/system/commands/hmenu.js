cde602fb63090a0864f317861c44ccb45b4502d8
SamuraiBread <samuraibreadi@gmail.com>
2025-10-15 22:43:25 +0200

    Create hmenu.js

---
+++ BP/scripts/system/commands/hmenu.js
@@ -0,0 +1,22 @@
+import { system, world, CommandPermissionLevel } from "@minecraft/server";
+import { ActionFormData } from "@minecraft/server-ui"
+
+import { hsmpMenu_MAIN } from "../menu/main-menu.js"
+
+system.beforeEvents.startup.subscribe(e => {
+    const registry = e.customCommandRegistry;
+
+    registry.registerCommand({
+        name: "hsmp:hmenu",
+        description: "Opens HiraethSMP menu.",
+        permissionLevel: CommandPermissionLevel.Any,
+        optionalParameters: []
+    },
+        (origin, args) => {
+            const player = origin.sourceEntity;
+            system.run(() => {
+                hsmpMenu_MAIN(player)
+            })
+        }
+    )
+})
