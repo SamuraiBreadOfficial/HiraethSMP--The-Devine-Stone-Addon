d76e4a268ef4a41f52642c51a94ac53a75bfca4a
SamuraiBread <samuraibreadi@gmail.com>
2025-10-15 22:42:33 +0200

    Create addon-info.js

---
+++ BP/scripts/addon-info.js
@@ -0,0 +1,42 @@
+export const addon = {
+    version: {
+        no: `v0.0.400`,
+        title: `Commanding And Explaining`,
+        desc: `This update focuses on Commands, and transferring old menu based systems to commands.`,
+        released: `2025/10/20`,
+        nextver: `"Racefull Race Power - v0.1.0 (BETA release.)"`
+    },
+    clog: {
+        short: `Commands, Economy and Menus Overhaul. Bugs Fixes`,
+        full: `
+- Added over 20 commands with which you can control: Economy (/bal, /dep, /with)
+- Overhauled whole economy
+- Overhauled setup screen
+- Removed unwanted settings configuration
+- Removed Factions in order to make place to Player Based Faction Creation System (Info for the future: in Short PBFCS(HSMP System))
+- Fixed over 200 bugs and glitches. Addon is now playable.
+        `
+    },
+    credits: {
+        staff: {
+            owner: `- SamuraiBread (MC: SamuraiBreadI)`,
+            admins: `- AkuCrestfallen (MC: FatalCurse)
+- Amber (MC: ShadowTheBloodWold)`,
+            mods: `- Tammy (MC: Tam)
+- Drowsy (MC: DR0WSY)`
+        },
+        devs: {
+            scripting: `SamuraiBread`,
+            ost: `SamuraiBread & Drowsy`,
+            testers: `Amber, Aku, Luka`
+        }
+    },
+    history: `v0.0.0-earlyAlpha - 2025/07/20 "Initial Publish"
+v0.0.10-alpha - 2025/08/10 "Menu System"
+v0.0.11 - 2025/08/11 "Races and Settings"
+v0.0.100 - 2025/09/10 "New Version Numbering"
+v0.0.110 - 2025/09/15 "Races Overhaul, Factions"
+v0.0.200 - 2025/09/29 "Bug Fixes"
+v0.0.300 - 2025/09/30 "Public Pre-Release"
+v0.0.400 - 2025/10/20 "Commanding and Explaining" (Current)`
+}
