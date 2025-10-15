bd5daf390dc9b4ba8a8ff0f5a45c7dccd2844768
SamuraiBread <samuraibreadi@gmail.com>
2025-10-15 22:43:04 +0200

    Create balmenu.js

---
+++ BP/scripts/system/commands/balmenu.js
@@ -0,0 +1,27 @@
+import { world, system, CommandPermissionLevel } from "@minecraft/server";
+import { hsmpMenu_BAL, hsmpMenu_BALnotSettedUp } from "../menu/main-menu.js";
+import { hasScore, getScore } from "../../modules/economy/economy-scoreboards.js"
+
+system.beforeEvents.startup.subscribe(e => {
+    const registry = e.customCommandRegistry;
+
+    registry.registerCommand(
+        {
+            name: "hsmp:balmenu",
+            description: "Shows Balance Menu (OLD)",
+            permissionLevel: CommandPermissionLevel.Any
+        },
+        (origin, args) => {
+            const player = origin.sourceEntity;
+
+            system.run(() => {
+                if (!hasScore(player, 'balance')) {
+                    hsmpMenu_BALnotSettedUp(player);
+                } else {
+                    hsmpMenu_BAL(player)
+                }
+            })
+
+        }
+    )
+})
