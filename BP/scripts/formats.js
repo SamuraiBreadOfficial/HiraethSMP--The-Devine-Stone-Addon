51f5959d36ac46de76d5590d8fe10151a927cbdf
SamuraiBread <samuraibreadi@gmail.com>
2025-11-02 18:09:28 +0100

    Add functions to witch potions.

--- BP/scripts/formats.js
+++ BP/scripts/formats.js
@@ -38,3 +38,297 @@ export async function typeActionbar(player, character, text, delay = 1, pitch = 
         }
     }
 }
+
+export const textFormats = {
+    colors: {
+        black: "§0",
+        dark_blue: "§1",
+        dark_green: "§2",
+        dark_aqua: "§3",
+        dark_red: "§4",
+        dark_purple: "§5",
+        gold: "§6",
+        gray: "§7",
+        dark_gray: "§8",
+        blue: "§9",
+        green: "§a",
+        aqua: "§b",
+        red: "§c",
+        light_purple: "§d",
+        yellow: "§e",
+        white: "§f",
+        minecoin: "§g",
+        quartz: "§h",
+        iron: "§i",
+        netherite: "§j",
+        redstone: "§m",
+        copper: "§n",
+        gold: "§p",
+        emerald: "§q",
+        diamond: "§s",
+        lapis: "§t",
+        amethyst: "§u"
+
+    },
+    deco: {
+
+        obfuscated: "§k",
+        bold: "§l",
+        italic: "§o"
+    },
+    reset: "§r",
+    resetWithColor: "§r§f"
+}
+
+export async function typeTitleTitle(player, text, delay = 1, sound = "random.click", pitch = 1) {
+    for (let i = 1; i <= text.length; i++) {
+        const fragment = text.slice(0, i);
+        system.run(() => {
+            player.runCommand(`title @s title ${fragment}`)
+            player.runCommand(`playsound ${sound} @s ~ ~ ~ 1 ${pitch} 1`)
+        });
+
+        const char = text[i - 1];
+        if (char === "." || char === " ") {
+            await waitTicks(delay + 6)
+        } else {
+            await waitTicks(delay)
+        }
+    }
+}
+
+export const specialLang = {
+    i: "Va",
+    you: "Vu",
+    he: "Ve",
+    she: "Ver",
+    it: "Vo",
+    we: "Vi",
+    they: "Vyr",
+
+    my: "U'va",
+    your: "U'vu",
+    his: "U've",
+    her: "U'ver",
+    its: "U'vo",
+    our: "U'var",
+    their: "U'vyr",
+
+    mine: "U'vara",
+    yours: "U'vur",
+    hers: "U'veri",
+    ours: "U'vari",
+    theirs: "U'vyri",
+
+    is: "Sin",
+    are: "San",
+    was: "Sir",
+    were: "Sire",
+    be: "Se",
+    being: "Sev",
+    been: "Sevi",
+    am: "Sera",
+    do: "Sut",
+    does: "Suta",
+    did: "Sutas",
+    have: "Sara",
+    has: "Sare",
+    had: "Saren",
+    will: "Niv",
+    would: "Niva",
+    shall: "Niva'tash",
+    should: "Neva",
+    can: "Ne",
+    could: "Nev",
+    may: "Ter",
+    might: "Tera",
+    must: "Terro",
+    ought: "Tesso",
+    need: "Belva",
+    dare: "Berri",
+
+    in: "Yt",
+    out: "Ot",
+
+
+    so: "Uk",
+    and: "Gel",
+
+    what: "He",
+
+    upon: "Her",
+
+    darkness: "Vaidos",
+
+    hidden: "Gaeilo",
+
+    underneath: "Hakerikia",
+
+    another: "Umader",
+    more: "Misa",
+    never: "Naina",
+
+    day: "Sola",
+
+    see: "Visia",
+
+    higher: "Java",
+
+    the: "Ke",
+
+    truth: "Outa",
+
+    goddess: "Davia",
+    god: "Davi",
+    gods: "Davos",
+
+    heal: "Sano",
+
+    live: "Hashi",
+    heart: "Hasho",
+    soul: "Si'Sila",
+
+    yes: "Yi",
+
+    no: "Ne",
+
+    oh: "Eek",
+    ah: "Eee",
+
+    please: "Gieva Vu",
+
+    thank: "Pradiash",
+    not: "Nei",
+
+    fix: "Rea",
+
+    fire: "Crim",
+    flame: "Crimo",
+
+    pain: "Cra'ri",
+    die: "Cra",
+
+    shield: "Berk",
+
+    than: "Kan",
+    as: "Ki",
+    like: "Re",
+
+    for: "Ko",
+    because: "Kuha'tash",
+    since: "Min",
+
+    if: "Ik",
+    when: "Kien",
+
+    with: "Zin",
+    without: "Zina",
+
+    at: "Re",
+    on: "Ro",
+    by: "Ra",
+    from: "Katosh",
+    to: "Ru",
+
+    all: "Hailvo",
+    each: "Desa",
+    every: "Desa'vo",
+    some: "Desi",
+    many: "Desu",
+
+    who: "Ze",
+    which: "Zir",
+    that: "Har",
+
+    just: "Ek",
+    already: "Usatrav",
+    still: "Beilo",
+    yet: "Pailo",
+
+    come: "Filia",
+
+    go: "Fili",
+
+    say: "Vois",
+
+    speak: "Vais",
+
+    tell: "Veis",
+
+    know: "Lumi",
+
+    think: "Seni",
+
+    want: "Devilo",
+
+    give: "Heishe",
+
+    take: "Shtelo",
+
+    look: "Visio",
+    watch: "Visiona",
+
+    now: "Ba",
+
+    then: "Jeish",
+
+    this: "Kela",
+
+    today: "Yt Kela Ke Sola",
+
+    tomorrow: "Yt Umader Ke Sola",
+
+    yesterday: "Yt Hakerikia Kela Ke Sola",
+
+    always: "Avoni",
+
+    often: "Havoni",
+
+    big: "Grandi",
+    large: "Grandes",
+
+    small: "Tinis",
+    little: "Tiandes",
+
+    good: "Hai",
+
+    bad: "Kei",
+
+    hot: "Yarva",
+
+    cold: "Cialra",
+
+    dark: "Vaid",
+
+    light: "Salas",
+
+    high: "Jav",
+
+    low: "Vaj",
+
+    these: "Harie",
+
+    those: "Haria",
+
+    much: 'Desas',
+
+    or: "Ar",
+
+    but: "Kek",
+
+    toward: "Ruwi",
+    by: "Sai"
+
+
+}
+
+export function translate(text) {
+    return text
+        .split(" ")
+        .map(word => {
+            const clean = word.toLowerCase().replace(/[^a-z]/gi, ""); // usuń interpunkcję
+            const translated = specialLang[clean];
+            return translated ? word.replace(clean, translated) : word;
+        })
+        .join(" ");
+}
--- BP/scripts/item/witch_potions.js
+++ BP/scripts/item/witch_potions.js
@@ -1,4 +1,5 @@
 import { system } from "@minecraft/server";
+import { formatCurrency, textFormats, waitTicks, typeActionbar, typeTitleTitle, specialLang, translate } from "./../formats.js"
 
 system.beforeEvents.startup.subscribe(e => {
     e.itemComponentRegistry.registerCustomComponent('hsmp:witch_potions', {
@@ -18,31 +19,41 @@ system.beforeEvents.startup.subscribe(e => {
             const weakness = "hsmp:weakness_witch_potion"
             const item = e.itemStack
             const player = e.source
-
+            player.runCommand(`title @s times 0 20 10`)
             player.startItemCooldown("hsmp:store_exclusive", 200)
 
             if (item.typeId == fireResistance) {
                 player.sendMessage(`FireRes`)
+                player.addEffect("fire_resistance", 9600, {
+                    amplifier: 3,
+                    showParticles: true
+                })
+                typeTitleTitle(player, textFormats.deco.bold + textFormats.deco.italic + textFormats.colors.redstone + translate(`our fire is your shield`), 3, "mob.blaze.breathe", 0.5)
             }
             if (item.typeId == harming) {
                 player.sendMessage(`harming`)
+                typeTitleTitle(player, textFormats.colors.dark_red + translate(`your pain will \n not fix your flame`), 3, "mob.blaze.breathe", 0.5)
             }
             if (item.typeId == healing) {
                 player.sendMessage(`heal`)
+                typeTitleTitle(player, textFormats.deco.bold + textFormats.colors.redstone + translate(`may gods heal your heart`))
             }
             if (item.typeId == invisibility) {
                 player.sendMessage(`invis`)
+                typeTitleTitle(player, translate(`shall they never \n see the truth`))
             }
             if (item.typeId == leaping) {
                 player.sendMessage(`leaping`)
+                typeTitleTitle(player, translate(`the higher the soul \n the more they will see`))
 
             }
             if (item.typeId == nightVision) {
                 player.sendMessage(`nightvis`)
-
+                typeTitleTitle(player, translate(`upon darkness , you must see \n what is hidden , \n underneath`))
             }
             if (item.typeId == poison) {
                 player.sendMessage(`poison`)
+                typeTitleTitle(player, translate(`and shall they die in pain \n so we can live \n another day`))
 
             }
             if (item.typeId == regen) {
