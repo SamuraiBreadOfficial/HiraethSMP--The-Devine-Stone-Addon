ba05b2324a2f050dd2a2e8ed7a754149d0f8b9be
SamuraiBread <samuraibreadi@gmail.com>
2025-12-07 19:59:30 +0100

    Add default return values for race and skill tags
    Updated getRaceTags and getMagicTags to return default messages when no matching tags are found. Also changed the demon race tag label to 'TANAR'RI' for consistency.

--- BP/scripts/modules/playerinfo/registered-tags.js
+++ BP/scripts/modules/playerinfo/registered-tags.js
@@ -9,13 +9,15 @@ export function getRaceTags(player) {
     if (player.hasTag(tag_human)) return `§lHUMAN§r`;
     if (player.hasTag(tag_elf)) return `§l§qELF§r`;
     if (player.hasTag(tag_half_orc)) return `§v§lHALF ORC§r`;
-    if (player.hasTag(tag_demon)) return `§l§mDEMON§r`;
+    if (player.hasTag(tag_demon)) return `§l§mTANAR'RI§r`;
+    return `No Race Found.`
 }
+
 
 export function getMagicTags(player) {
     if (player.hasTag(tag_fire)) return "Crimsoning Flames"
     if (player.hasTag(tag_wind)) return "Titan's Breath"
     if (player.hasTag(tag_earth)) return "Earth's Will"
     if (player.hasTag(tag_nature)) return "Flowering Blessing"
-
+    return `No skills found.`
 }
